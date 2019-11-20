2be04b8fb2c291bd2a6764c30b3b1fcba805cfaf
pagecow <pagecow@gmail.com>
2019-11-20 12:31:50 -0700

    updated

---
+++ src/Components/HomeProductPage/HomeProductHeaderOne.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+function HomeProductHeaderTwo() {
+    return(  
+        <div id='home-product-page-header-1'>
+            <img id='logo' src='https://static.thenounproject.com/png/337525-200.png'/> 
+        </div> 
+    )
+}
+
+export default HomeProductHeaderTwo;
