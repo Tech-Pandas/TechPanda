18da6235940a91032947f63b157cee724c7b5185
pagecow <pagecow@gmail.com>
2019-11-22 15:30:14 -0700

    look updated

--- src/Components/HomeProductPage/HomeProductPage.js
+++ src/Components/HomeProductPage/HomeProductPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import ReactPlayer from 'react-player';
 import "./HomeProductPage.css"
 
@@ -7,6 +7,8 @@ import HomeProductHeaderOne from './HomeProductHeaderOne';
 import HomeProductHeaderTwo from './HomeProductHeaderTwo';
 
 function HomeProductPage() {
+    
+    
     return(
         <div>
             <HomeProductHeaderOne/>
