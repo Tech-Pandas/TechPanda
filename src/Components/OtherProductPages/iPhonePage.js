03022df78e4d04fc434b1b3331e9f15a62cc954f
Nathaniel Lawrence <nathanielawrence@gmail.com>
2019-11-26 14:04:17 -0700

    iphone11

---
+++ src/Components/OtherProductPages/iPhonePage.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
+import ReactPlayer from 'react-player';
+import './Iphone.css';
+
+// Little helpers ...
+// const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
+// const Pink = ({ children }) => <span style={{ color: '#FF6AC1' }}>{children}</span>
+// const Yellow = ({ children }) => <span style={{ color: '#EFF59B' }}>{children}</span>
+// const Lightblue = ({ children }) => <span style={{ color: '#9AEDFE' }}>{children}</span>
+// const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</span>
+// const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span>
+// const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
+
+class IphonePage extends Component {
+  render() { 
+    return (
+      <Parallax ref={ref => (this.parallax = ref)} pages={3} className='containpro'>
+      <div>
+        <ReactPlayer
+        url="https://www.apple.com/105/media/us/iphone-11-pro/2019/3bd902e4-0752-4ac1-95f8-6225c32aec6d/anim/hero/small_2x.mp4" type="video/mp4" src="https://www.apple.com/105/media/us/iphone-11-pro/2019/3bd902e4-0752-4ac1-95f8-6225c32aec6d/anim/hero/small_2x.mp4" alt='vid' playing muted width='100%' height='100%' 
+        />
+      </div>
+
+
+      {/* <div className='thisone' > */}
+        <ParallaxLayer offset={1} speed={3} className='therewasprocontainerwhite' style={{ backgroundColor: 'rgba(0,0,0,1)' }}>
+
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={-3} className='therewasprocontainer' style={{ backgroundColor: 'rgba(0,0,0,1)', backgroundImage: 'https://www.apple.com/v/iphone-11-pro/c/images/overview/camera/camera_three_lens_system_phone__cokiuc6tixw2_large.jpg', backgroundSize: 'cover', height: '50', color: 'white'}}>
+          <h1 className='therewaspro'>And then there was Pro.</h1>
+          <h3 className='therewaspro1'>A transformative triple‑camera system that adds tons of capability without complexity.
+          </h3>
+          <h3 className='therewaspro1'>An unprecedented leap in battery life. And a mind‑blowing chip</h3>
+          <h3 className='therewaspro1'>that doubles down on machine learning</h3>
+          <h3 className='therewaspro1'>and pushes the boundaries of what a smartphone can do.</h3>
+          <h3 className='therewaspro1'>
+          Welcome to the first iPhone powerful enough to be called Pro.
+          </h3>
+        </ParallaxLayer>
+        IPHONENNENNENENENNENENNEE
+        
+        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: 'gray' }} />
+      {/* </div>   */}
+
+        {/* <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: ('https://www.apple.com/v/iphone-11-pro/c/images/overview/hero/triptych_left_landscape__eczd4p0wyv6u_small_2x.jpg', true), backgroundSize: 'cover' }} /> */}
+      </Parallax>
+    )
+  }
+}
+
+export default IphonePage;
